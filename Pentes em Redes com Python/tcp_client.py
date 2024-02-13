3d7e91d8ffeaa99b44682db2da1ade70b64f17cc
LucasHenriqueee <84096509+LucasHenriqueee@users.noreply.github.com>
2024-02-13 09:56:30 -0300

    Create tcp_client.py

---
+++ Pentes em Redes com Python/tcp_client.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# tcp_client.py
+
+import socket
+
+HOST = 'www.google.com'
+PORT = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 1 - criando um objeto socket
+client.connect((HOST, PORT)) #conecte o cliente
+
+client.send(b'GET / HTTP/1.1\r\nHost: google.com\r\n\r\n') # envie algum dado
+response = client.recv(4096) # recebemos algum tipo de dado
+print(response.decode('utf-8')) # imprimimos atela
+
+client.close()a
